abfc5e417c3f625334702d3efddc3de1fcd1217c
danielle-gg <gomezdan@sheridancollege.ca>
2024-02-06 11:58:59 -0300

    add class

---
+++ src/StudentListDemo/PartTimeStudent.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StudentListDemo;
+
+/**
+ *
+ * @author Danielle
+ */
+public class PartTimeStudent extends Student{
+    
+    public int getNumCourses() {
+        return numCourses;
+    }
+    
+    /*not finished*/
+}
